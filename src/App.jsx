6cba332f23e345060489f2a13e13c695914b435d
hashinmohammed <hashinpks@gmail.com>
2023-07-12 22:46:32 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import Login from './pages/Login';
+import CityList from './components/CityList';
+import {useState} from 'react'
 
+const BASE_URL= 'http://localhost:9000'
 const App = () => {
-  const x=23;
-  
+const [cities,setCities]=useState([])
+const [isLoading,setIsLoading]=useState(false)
+  useEffect(()=>{
+    const fetchCities=async ()=>{
+      try {
+        setIsLoading(true)
+        const res=await fetch(`${BASE_URL}/cities`)
+        const data=await res.json()
+        setCities(data)
+        console.log(data);
+      } catch (error) {
+        console.log('there was an error');
+      }
+      finally{
+        setIsLoading(false)
+      }
+    }
+    fetchCities()
+  },[])
   return (
 <BrowserRouter>
 <Routes>
-<Route path='/' element={<Homepage />}/>
+<Route index element={<Homepage />}/>
 <Route path='product' element={<Product/> }/>
 <Route path='pricing' element={ <Pricing/> }/>
-<Route path='app' element={ <AppLayout/> }/>
+<Route path='login' element={<Login/> }/>
+
+<Route path='app' element={ <AppLayout/> }>
+<Route index element={<CityList isLoading={isLoading} cities={cities}/>} />
+
+  <Route path='cities' element={<CityList isLoading={isLoading} cities={cities}/>} />
+  <Route path='countries' element={<p>countries</p>} />
+  <Route path='form' element={<p>countries</p>} />
+
+
+</Route>
 
 <Route path='*' element={ <PageNotFound/> }/>
 
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
-import styles from './AppNav.module.css'
-const AppNav = () => {
+import { NavLink } from "react-router-dom";
+import styles from "./AppNav.module.css";
+
+function AppNav() {
   return (
-    <nav className={styles.nav}>App navigation</nav>
-  )
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
-export default AppNav
+export default AppNav;
